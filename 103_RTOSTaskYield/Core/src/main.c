79a6dc7a69caec25919b913b44195b0efd6629f9
ahasanzadeh <hasanzadeh.amin@gmail.com>
2024-06-23 11:39:50 -0700

    Major modifications

--- 103_RTOSTaskYield/Core/src/main.c
+++ 103_RTOSTaskYield/Core/src/main.c
@@ -1,9 +1,9 @@
-#include "stm32f4xx.h"
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
 #include "FreeRTOS.h"
+#include "stm32f4xx.h"
 #include "task.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 TaskHandle_t xTask1_Handler = NULL;
 TaskHandle_t xTask2_Handler = NULL;
@@ -21,25 +21,24 @@ extern void initialise_monitor_handles();
 static void prvSetupUart(void);
 static void prvSetupHardware(void);
 
-void USART_SendText(USART_TypeDef* USARTx, const char *sendText);
+void USART_SendText(USART_TypeDef *USARTx, const char *sendText);
 
 /*Macros*/
-#define TRUE                  1
-#define FALSE                 0
-#define AVAILABLE_TO_RUN      TRUE 
-#define NOT_AVAILABLE_TO_RUN  FALSE 
+#define TRUE 1
+#define FALSE 0
+#define AVAILABLE_TO_RUN TRUE
+#define NOT_AVAILABLE_TO_RUN FALSE
 
 /*Global variables section*/
-char userText[99]; 
+char userText[99];
 uint16_t uartAccessKey = AVAILABLE_TO_RUN;
 
-int main(void)
-{ 
-  #ifdef SEMI_HOSTING
+int main(void) {
+#ifdef SEMI_HOSTING
   /*Enable semi-hosting*/
   initialise_monitor_handles();
   printf("This is semi-hosting test for FreeRTOS project\n");
-  #endif
+#endif
 
   /*Reset the RCC clock configuration to the default reset state.
   Only HSI  is turned on, and uses as system clock source as HSI= 16MHz*/
@@ -55,32 +54,29 @@ int main(void)
   USART_SendText(USART3, userText);
 
   /*Create tasks for RTOS*/
-  xTaskCreate(vTask1_Function, "Task1", configMINIMAL_STACK_SIZE, NULL, 2, &xTask1_Handler);
- 
-  xTaskCreate(vTask2_Function, "Task2", configMINIMAL_STACK_SIZE, NULL, 2, &xTask2_Handler);
- 
+  xTaskCreate(vTask1_Function, "Task1", configMINIMAL_STACK_SIZE, NULL, 2,
+              &xTask1_Handler);
+
+  xTaskCreate(vTask2_Function, "Task2", configMINIMAL_STACK_SIZE, NULL, 2,
+              &xTask2_Handler);
+
   /*Start the scheduler*/
   vTaskStartScheduler();
 
   /*By addig scheduler the below code will never be executed
-  since above scheduler always runs, resulting in functions 
+  since above scheduler always runs, resulting in functions
   runs where functions will never return*/
-  while(1)
-  { 
-
+  while (1) {
   }
   return 0;
 }
 
-void vTask1_Function(void *funcParams)
-{
-  while(1)
-  {
-    #ifdef SEMI_HOSTING
+void vTask1_Function(void *funcParams) {
+  while (1) {
+#ifdef SEMI_HOSTING
     printf("Task 1 function\n");
-    #endif
-    if (uartAccessKey == AVAILABLE_TO_RUN)
-    {
+#endif
+    if (uartAccessKey == AVAILABLE_TO_RUN) {
       uartAccessKey = NOT_AVAILABLE_TO_RUN;
       USART_SendText(USART3, "Task 1 function\r\n");
       uartAccessKey = AVAILABLE_TO_RUN;
@@ -90,15 +86,12 @@ void vTask1_Function(void *funcParams)
   }
 }
 
-void vTask2_Function(void *funcParams)
-{
-  while(1)
-  {
-    #ifdef SEMI_HOSTING 
+void vTask2_Function(void *funcParams) {
+  while (1) {
+#ifdef SEMI_HOSTING
     printf("Task 2 function\n");
-    #endif
-    if (uartAccessKey == AVAILABLE_TO_RUN)
-    {
+#endif
+    if (uartAccessKey == AVAILABLE_TO_RUN) {
       uartAccessKey = NOT_AVAILABLE_TO_RUN;
       USART_SendText(USART3, "Task 2 function\r\n");
       uartAccessKey = AVAILABLE_TO_RUN;
@@ -108,30 +101,29 @@ void vTask2_Function(void *funcParams)
   }
 }
 
-static void prvSetupUart(void)
-{
+static void prvSetupUart(void) {
   /*Virtual COM Port: USART_3 (USART_C_TX: PD8, USART_C_RX: PD9)*/
   /* Initiate clock for GPIOD */
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-  
+
   /* Initiate GPIOD */
   GPIO_InitTypeDef GPIO_InitStruct;
 
   /*Reset every member element of the structure*/
   memset(&GPIO_InitStruct, 0, sizeof(GPIO_InitStruct));
-    
-  GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_8 | GPIO_Pin_9;
+
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-  
+
   GPIO_Init(GPIOD, &GPIO_InitStruct);
 
   /*Connect GPIOD pins to AF for USART3*/
   GPIO_PinAFConfig(GPIOD, GPIO_PinSource8, GPIO_AF_USART3);
   GPIO_PinAFConfig(GPIOD, GPIO_PinSource9, GPIO_AF_USART3);
-  
+
   /*Initialize USART3 clock*/
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
 
@@ -141,12 +133,12 @@ static void prvSetupUart(void)
   /*Reset every member element of the structure*/
   memset(&USART_InitStruct, 0, sizeof(USART_InitStruct));
 
-  USART_InitStruct.USART_BaudRate = 115200;
+  USART_InitStruct.USART_BaudRate = 9600;
   USART_InitStruct.USART_WordLength = USART_WordLength_8b;
   USART_InitStruct.USART_StopBits = USART_StopBits_1;
-  USART_InitStruct.USART_Parity = USART_Parity_No ;
+  USART_InitStruct.USART_Parity = USART_Parity_No;
   USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;  
+  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 
   /*Initialize USART3*/
   USART_Init(USART3, &USART_InitStruct);
@@ -155,16 +147,12 @@ static void prvSetupUart(void)
   USART_Cmd(USART3, ENABLE);
 }
 
-static void prvSetupHardware(void)
-{
-  prvSetupUart();
-}
+static void prvSetupHardware(void) { prvSetupUart(); }
 
-void USART_SendText(USART_TypeDef* USARTx, const char *sendText)
-{
-  for (uint32_t i = 0; i < strlen(sendText); i++)
-  {
-    while(USART_GetFlagStatus(USARTx, USART_FLAG_TXE) != SET);
+void USART_SendText(USART_TypeDef *USARTx, const char *sendText) {
+  for (uint32_t i = 0; i < strlen(sendText); i++) {
+    while (USART_GetFlagStatus(USARTx, USART_FLAG_TXE) != SET)
+      ;
     USART_SendData(USARTx, sendText[i]);
   }
 }
--- 107_RTOSTaskWaitNotifyAction/Core/src/main.c
+++ 107_RTOSTaskWaitNotifyAction/Core/src/main.c
@@ -1,9 +1,9 @@
-#include "stm32f4xx.h"
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
 #include "FreeRTOS.h"
+#include "stm32f4xx.h"
 #include "task.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 TaskHandle_t xTask1_Handler = NULL;
 TaskHandle_t xTask2_Handler = NULL;
@@ -24,25 +24,24 @@ static void GPIO_Setup(void);
 static void VirtualCOMPort_Setup(void);
 void rtosDelayMs(uint32_t ms);
 
-void USART_SendText(USART_TypeDef* USARTx, volatile char *sendText);
-void USART_SendNumber(USART_TypeDef* USARTx, uint32_t sendNumber);
+void USART_SendText(USART_TypeDef *USARTx, volatile char *sendText);
+void USART_SendNumber(USART_TypeDef *USARTx, uint32_t sendNumber);
 
 /*Macros section*/
-#define TRUE                    1
-#define FALSE                   0
-#define PUSH_BUTTON_PRESSED     TRUE 
-#define PUSH_BUTTON_NOT_PRESSED FALSE 
+#define TRUE 1
+#define FALSE 0
+#define PUSH_BUTTON_PRESSED TRUE
+#define PUSH_BUTTON_NOT_PRESSED FALSE
 
 /*Global variables section*/
 uint8_t pushButtonStatusFlag = PUSH_BUTTON_NOT_PRESSED;
 
-int main(void)
-{ 
-  #ifdef SEMI_HOSTING
+int main(void) {
+#ifdef SEMI_HOSTING
   /*Enable semi-hosting*/
   initialise_monitor_handles();
   printf("This is semi-hosting test for FreeRTOS project\n");
-  #endif
+#endif
 
   /*Reset the RCC clock configuration to the default reset state.
   Only HSI  is turned on, and uses as system clock source as HSI= 16MHz*/
@@ -58,43 +57,39 @@ int main(void)
   /*Increase stack size to 500 wods instead of 130words=configMINIMAL_STACK_SIZE
   since we are going to use FreeRTOS API,and tasks use higher stack memory*/
   xTaskCreate(LED_task_Function, "LED_task", 500, NULL, 2, &xTask1_Handler);
- 
-  xTaskCreate(PushButton_task_Function, "PushButton_task", 500, NULL, 2, &xTask2_Handler);
- 
+
+  xTaskCreate(PushButton_task_Function, "PushButton_task", 500, NULL, 2,
+              &xTask2_Handler);
+
   /*Start the scheduler*/
   vTaskStartScheduler();
 
   /*By addig scheduler the below code will never be executed
-  since above scheduler always runs, resulting in functions 
+  since above scheduler always runs, resulting in functions
   runs where functions will never return*/
-  while(1)
-  { 
+  while (1) {
   }
   return 0;
 }
 
-void LED_task_Function(void *funcParams)
-{
+void LED_task_Function(void *funcParams) {
   uint32_t currentNotificatonValue = 0;
-  while(1)
-  {
+  while (1) {
     /*Wait until any notification event from button task received*/
-    if (xTaskNotifyWait(0, 0, &currentNotificatonValue, portMAX_DELAY) ==pdTRUE)
-    {
+    if (xTaskNotifyWait(0, 0, &currentNotificatonValue, portMAX_DELAY) ==
+        pdTRUE) {
       GPIO_ToggleBits(GPIOB, GPIO_Pin_0 | GPIO_Pin_7 | GPIO_Pin_14);
-      USART_SendText(USART3, "Notification is received, Push Button press count: %ld\t");
+      USART_SendText(USART3,
+                     "Notification is received, Push Button press count:\t");
       USART_SendNumber(USART3, currentNotificatonValue);
       USART_SendText(USART3, "\n");
     }
   }
 }
 
-void PushButton_task_Function(void *funcParams)
-{
-  while(1)
-  {
-    if(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13))
-    {
+void PushButton_task_Function(void *funcParams) {
+  while (1) {
+    if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13)) {
       rtosDelayMs(100);
       xTaskNotify(xTask1_Handler, 0x0, eIncrement);
       USART_SendText(USART3, "Notification is sent\r\n");
@@ -102,19 +97,20 @@ void PushButton_task_Function(void *funcParams)
   }
 }
 
-static void Peripheral_Setup(void)
-{
+static void Peripheral_Setup(void) {
   RCC_Setup();
   GPIO_Setup();
   VirtualCOMPort_Setup();
 }
 
-static void RCC_Setup()
-{
+static void RCC_Setup() {
   /*Initialize GPIOB for toggling LEDs*/
-  /*USART_2 (USART_B_RX: PD6 D52 on CN9, USART_B_TX: PD5 D53 on CN9) & USART_3 (USART_A_TX: PD8, USART_A_RX: PD9)*/
+  /*USART_2 (USART_B_RX: PD6 D52 on CN9, USART_B_TX: PD5 D53 on CN9) & USART_3
+   * (USART_A_TX: PD8, USART_A_RX: PD9)*/
   /* Initiate clock for GPIOB, GPIOC, GPIOD, and GPIOG */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOD, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_GPIOC |
+                             RCC_AHB1Periph_GPIOD,
+                         ENABLE);
 
   /*Initialize USART3 clock*/
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
@@ -123,31 +119,30 @@ static void RCC_Setup()
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 }
 
-static void GPIO_Setup()
-{
+static void GPIO_Setup() {
   /* Initialize GPIOB*/
   GPIO_InitTypeDef GPIO_InitStruct;
 
   /*Reset every member element of the structure*/
   memset(&GPIO_InitStruct, 0, sizeof(GPIO_InitStruct));
 
-  GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_0 | GPIO_Pin_7 | GPIO_Pin_14;
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_7 | GPIO_Pin_14;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  
+
   GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* Initiate GPIOC */
-  GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_13;
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
-  
+
   GPIO_Init(GPIOC, &GPIO_InitStruct);
-  #if 0
+#if 0
   /*Configure interrupt for PC13*/
   SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOC, EXTI_PinSource13);
 
@@ -164,17 +159,17 @@ static void GPIO_Setup()
   /*Configure IRQ*/
   NVIC_SetPriority(EXTI15_10_IRQn, 5);
   NVIC_EnableIRQ(EXTI15_10_IRQn);
-  #endif
+#endif
   /* Initialize GPIOD*/
-  GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_8 | GPIO_Pin_9;
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-  
+
   GPIO_Init(GPIOD, &GPIO_InitStruct);
 }
-  #if 0
+#if 0
 void EXTI15_10_IRQHandler(void)
 {
   /*Cleas the interrupt pending register (PR) bit*/
@@ -183,8 +178,7 @@ void EXTI15_10_IRQHandler(void)
 
 }
 #endif
-static void VirtualCOMPort_Setup(void)
-{
+static void VirtualCOMPort_Setup(void) {
   /*USART_3 (USART_C_TX: PD8, USART_C_RX: PD9) has virtual COM port capability*/
 
   /*Configure USART3*/
@@ -201,9 +195,9 @@ static void VirtualCOMPort_Setup(void)
   USART_InitStruct.USART_BaudRate = 9600;
   USART_InitStruct.USART_WordLength = USART_WordLength_8b;
   USART_InitStruct.USART_StopBits = USART_StopBits_1;
-  USART_InitStruct.USART_Parity = USART_Parity_No ;
+  USART_InitStruct.USART_Parity = USART_Parity_No;
   USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;  
+  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 
   /*Initialize USART3*/
   USART_Init(USART3, &USART_InitStruct);
@@ -218,58 +212,52 @@ static void VirtualCOMPort_Setup(void)
   NVIC_EnableIRQ(USART3_IRQn);
 }
 
-void USART3_IRQHandler(void)
-{
-  if (USART_GetITStatus(USART3, USART_IT_RXNE))
-  {
-    if (USART_ReceiveData(USART3) == 'K')
-    {
+void USART3_IRQHandler(void) {
+  if (USART_GetITStatus(USART3, USART_IT_RXNE)) {
+    if (USART_ReceiveData(USART3) == 'K') {
       GPIO_ToggleBits(GPIOB, GPIO_Pin_0 | GPIO_Pin_7 | GPIO_Pin_14);
-      
+
       USART_SendText(USART3, "LED Toggled\n");
     }
   }
 }
 
-void USART_SendText(USART_TypeDef* USARTx, volatile char *sendText)
-{
-  while(*sendText)
-  {
-    while(USART_GetFlagStatus(USARTx, USART_FLAG_TXE) != SET);
+void USART_SendText(USART_TypeDef *USARTx, volatile char *sendText) {
+  while (*sendText) {
+    while (USART_GetFlagStatus(USARTx, USART_FLAG_TXE) != SET)
+      ;
     USART_SendData(USARTx, *sendText);
     *sendText++;
   }
 }
 
-void USART_SendNumber(USART_TypeDef* USARTx, uint32_t sendNumber)
-{
+void USART_SendNumber(USART_TypeDef *USARTx, uint32_t sendNumber) {
   /*A temp array to build results of conversion*/
   char value[10];
   /*Loop index*/
   int i = 0;
 
-  do
-  {
+  do {
     /*Convert integer to character*/
     value[i++] = (char)(sendNumber % 10) + '0';
     sendNumber /= 10;
   } while (sendNumber);
-  
+
   /*Send data*/
-  while(i)
-  {
-    //USART_SendNumber8b(USARTx, value[--i]);
+  while (i) {
+    // USART_SendNumber8b(USARTx, value[--i]);
     /*Wait until data register is empty*/
-    while (!USART_GetFlagStatus(USARTx, USART_FLAG_TXE));
-    USART_SendData(USARTx, value[--i]); 
+    while (!USART_GetFlagStatus(USARTx, USART_FLAG_TXE))
+      ;
+    USART_SendData(USARTx, value[--i]);
   }
 }
 
-void rtosDelayMs(uint32_t ms)
-{
+void rtosDelayMs(uint32_t ms) {
   uint32_t curretTickCount = xTaskGetTickCount();
 
-  uint32_t msTicks= (ms * configTICK_RATE_HZ) / 1000;
+  uint32_t msTicks = (ms * configTICK_RATE_HZ) / 1000;
 
-  while(xTaskGetTickCount() < (curretTickCount + msTicks));
+  while (xTaskGetTickCount() < (curretTickCount + msTicks))
+    ;
 }
