0fac804353372195d42f1c7d428ccd6cce3229cb
ahasanzadeh <hasanzadeh.amin@gmail.com>
2024-12-28 18:57:46 -0500

    Segger SystemView was upgraded to V358

--- 100_FreeRTOSEmpty/Core/src/main.c
+++ 100_FreeRTOSEmpty/Core/src/main.c
@@ -2,10 +2,16 @@
 #include <stdio.h>
 
 int main(void)
-{ 
-  while(1)
-  { 
+{
+    /*Reset the RCC clock configuration to the default reset state.
+    Only HSI  is turned on, and uses as system clock source as HSI= 16MHz*/
+    RCC_DeInit();
 
-  }
-  return 0;
+    /*Update SytemCoreClock variable to 16MHz*/
+    SystemCoreClockUpdate();
+
+    while (1)
+    {
+    }
+    return 0;
 }
